cce3bf21f3bbead13aa81e5cf97d070d3b0299e5
jamil <jamil@LAPTOP-N832HLDJ>
2024-04-29 01:54:35 -0300

    Adicionar arquivos de projeto.

---
+++ DesafioDiamante/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace DesafioDiamante
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tamanhoDiamante, metadeDiamante;
+
+            Console.WriteLine("Informe o tamanho do diamante (ímpar): ");
+            tamanhoDiamante = Convert.ToInt32(Console.ReadLine());
+
+            if(tamanhoDiamante % 2 == 0)
+            {
+                Console.WriteLine("\nValor inválido! Insira um número ímpar.");
+                return;
+            }
+
+            metadeDiamante = tamanhoDiamante / 2;
+
+            for(int i = 0; i <= metadeDiamante; i++)
+            {
+                for (int j = 0; j < metadeDiamante - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for(int j = 0; j < 2 * i + 1; j++)
+                {
+                    Console.Write("x");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = metadeDiamante - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < metadeDiamante - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < 2 * i + 1; j++)
+                {
+                    Console.Write("x");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
